77fef43ee28a397d174620adf3bc5aabbdef9970
theshine-dev <theshine.dev@gmail.com>
2022-04-22 18:11:15 +0900

    응답코드 기능 추가

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+import enum
+
 from Kakao import meal
 
 app = FastAPI()
@@ -14,21 +16,28 @@ app = FastAPI()
 '''
 
 
-@app.get("Kakao")
+class ResponseCode(enum.IntEnum):
+    OK = 200
+    CommandError = 100
+    ArgumentError = 110
+
+
+@app.get("/kakao")
 async def kakao(room: str, msg: str, sender: str, isGroupChat: bool, replier, imageDB: str, packageName: str):
     msg = msg.split(" ", 1)
     command = msg[0]
-    result = "알 수 없는 명령어 :("
+    result = {
+        "code": ResponseCode.CommandError,
+        "msg": "알 수 없는 명령어 :(",
+    }
     if command == "급식":
         print("".join(msg[1:]))
-        result = await meal.getMeal("".join(msg[1:]))
+        result["code"] = ResponseCode.OK
+        result["msg"] = await meal.getMeal("".join(msg[1:]))
 
     print(f'{room, msg, sender, isGroupChat, replier, imageDB, packageName}')
-    return {"result": result}
-
+    return result
 
 # @app.get("/hello/{name}")
 # async def say_hello(name: str):
 #     return {"message": f"Hello {name}"}
-
-
